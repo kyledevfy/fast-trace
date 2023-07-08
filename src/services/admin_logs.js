4626cf4d59616b344832d3628819201e7f394644
Kyle Joseph Timajo <timajokyle24@gmail.com>
2023-07-08 14:29:09 +0800

    refactor: update baseURL

--- src/services/admin_logs.js
+++ src/services/admin_logs.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://vtrace-backend.onrender.com",
+    baseURL: "https://vtrace-backend.onrender.com/api",
     // baseURL: "http://localhost:5000/api",
     withCredentials: true,
 });
